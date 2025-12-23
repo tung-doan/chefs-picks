de74740f121d1cee64ff8168701f2d8caf75bcca
nhatbx4002 <nhatbui2004.work@gmail.com>
2025-12-23 11:49:25 +0700

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link ,useNavigate } from "react-router-dom";
 import { Loader2, AlertCircle } from "lucide-react";
 import Header from "../components/layout/Header";
 import { API_BASE_URL } from "../config/api-config";
